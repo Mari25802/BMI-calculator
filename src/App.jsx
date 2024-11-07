ef6232f77e2e37272309537ecee852f64828e011
Mariselvam <126177490+Mari25802@users.noreply.github.com>
2024-11-07 12:53:47 +0530

    after

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BMI from './assets/bmi_01.png'
 
 function App() {
   const[height,setHeight]=useState("")
